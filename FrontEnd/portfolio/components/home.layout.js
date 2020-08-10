b970996895a0d6fa97cd0a385bfcae81bc860a53
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-08-10 03:30:14 +0100

    More Tweaks
    - Changed Name
    - Layout of arrow and tags changed
    - Added Animation on down arrow

--- FrontEnd/portfolio/components/home.layout.js
+++ FrontEnd/portfolio/components/home.layout.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from './home.layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
-const name = 'Raven'
+const name = 'Raven Duffy'
 export const pageTitle = 'Raven Duffy'
 
 export default function Layout({children}) {
@@ -20,11 +20,6 @@ export default function Layout({children}) {
       <header className={styles.header}>
         <>
           <h1 className={`${styles.largeCenterHeader} ${utilStyles.noSelect}`}>{name}</h1>
-          <img
-            src="images/down.svg"
-            className={`${styles.downArrow}`}
-            alt={"down-arrow"}
-          />
         </>
       </header>
       <main>{children}</main>
--- FrontEnd/portfolio/pages/index.js
+++ FrontEnd/portfolio/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { pageTitle } from '../components/home.layout.js'
 import utilStyles from '../styles/utils.module.css'
+import styles from '../components/home.layout.module.css'
 
 // export async function getStaticProps() {
 //
@@ -13,6 +14,18 @@ export default function Home() {
       <Head>
         <title>{pageTitle}</title>
       </Head>
+      <div className={`${styles.rowContainer} ${utilStyles.noSelect}`}>
+        <h1>About</h1>
+        <h1 className={utilStyles.extraLeftMargin}>Work</h1>
+        <h1>Contact</h1>
+      </div>
+      <div className={styles.flexContainer}>
+        <img
+          src="images/down.svg"
+          className={`${styles.downArrow} ${utilStyles.noSelect}`}
+          alt={"down-arrow"}
+        />
+      </div>
     </Layout>
   )
 }
