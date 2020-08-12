e0b2018785003418128061074a85fcc921c113bd
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-08-12 02:31:22 +0100

    Transitioned CSS
    - The last commit meant that the nav arrows couldn't be used outside of the home page, this as been remidied using the utilStyles file

--- FrontEnd/portfolio/components/navarrow.js
+++ FrontEnd/portfolio/components/navarrow.js
@@ -1,14 +1,13 @@
 import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
-import styles from './home/layout.module.css'
 
 export default function NavArrow({target}) {
   return (
-    <div className={styles.flexCenterContainer}>
+    <div className={utilStyles.flexCenterContainer}>
       <Link href={`#${target}`}>
         <img
           src="images/down.svg"
-          className={`${styles.downArrow} ${utilStyles.noSelect}`}
+          className={`${utilStyles.downArrow} ${utilStyles.noSelect}`}
           alt={"down-arrow"}
         />
       </Link>
