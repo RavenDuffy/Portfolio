5120631cd81fad46220bed53ddd2308a5cf51dce
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-08-13 17:13:59 +0100

    More meta optimising
    - added OG and Twitter tags

--- FrontEnd/portfolio/components/home/layout.js
+++ FrontEnd/portfolio/components/home/layout.js
@@ -30,9 +30,18 @@ export default function Layout({children}) {
         <meta name="description" content={description} />
         <meta name="robots" content="index, follow" />
 
-        <meta itemprop="name" content={pageTitle} />
-        <meta itemprop="description" content={description} />
-        <meta itemprop="image" content="images/NoBackRaven.png" />
+        <meta property="og:url" content="http://ravenduffy.io" />
+        <meta property="og:image" content="http://ravenduffy.io/images/NoBackRaven.png" />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={name} />
+        <meta property="og:site_name" content={pageTitle} />
+        <meta property="og:see_also" content="http://ravenduffy.io" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="http://ravenduffy.io" />
+        <meta name="twitter:title" content={name} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="http://ravenduffy.io/images/NoBackRaven.png" />
       </Head>
       <div className={styles.hero}>
         <header className={styles.header}>
