e79046ed893c724cfa566980f0ecb1d176aa3553
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-08-15 02:32:03 +0100

    Abstracted Meta Tags

--- FrontEnd/portfolio/components/home/layout.js
+++ FrontEnd/portfolio/components/home/layout.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import dynamic from 'next/dynamic'
 import styles from './layout.module.css'
 import utilStyles from '../../styles/utils.module.css'
+import Meta from '../meta.js'
 import NavArrow from '../navarrow.js'
 import BackToTop from '../backtotop.js'
 
@@ -25,24 +26,7 @@ export default function Layout({children}) {
   return (
     <div className={styles.mainContainer}>
       <Head>
-        <link rel="icon" href="favicon.ico" />
-        <meta name="Raven Duffy" content="Raven Duffy's portfolio" />
-        <meta name="title" content={pageTitle} />
-        <meta name="description" content={description} />
-        <meta name="robots" content="index, follow" />
-
-        <meta property="og:url" content="http://ravenduffy.io" />
-        <meta property="og:image" content="http://ravenduffy.io/images/NoBackRaven.png" />
-        <meta property="og:description" content={description} />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:site_name" content={name} />
-        <meta property="og:see_also" content="http://ravenduffy.io" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="http://ravenduffy.io" />
-        <meta name="twitter:title" content={pageTitle} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="http://ravenduffy.io/images/NoBackRaven.png" />
+        <Meta name={name} pageTitle={pageTitle} description={description} />
       </Head>
       <div className={styles.hero}>
         <header className={styles.header}>
