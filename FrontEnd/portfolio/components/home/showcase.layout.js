42d6fd221bc970947006b11906a93b6726e3356f
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-08-20 16:44:36 +0100

    Update showcase.layout.js
    - was printing d in console, whoops

--- FrontEnd/portfolio/components/home/showcase.layout.js
+++ FrontEnd/portfolio/components/home/showcase.layout.js
@@ -3,7 +3,6 @@ import utilStyles from '../../styles/utils.module.css'
 import styles from './layout.module.css'
 
 export default function ShowcaseLayout({ home, allProjectData }) {
-  console.log("d")
   return (
     <div className={styles.section} id="showcase" data-background="#259ed9">
       { home ? (
