17ef75bd69b4de4752938dd91c163f2523ce0af6
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-08-24 21:07:26 +0100

    Back to home button done for now
    - finished work on back to home button (for now)

--- FrontEnd/portfolio/components/backtohome.js
+++ FrontEnd/portfolio/components/backtohome.js
@@ -3,14 +3,14 @@ import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 
 export default function BackToHome() {
-  let target
-  if (process.browser) target = document.location.pathname
-
   return (
-    <div className={`${utilStyles.noSelect} ${utilStyles.backToHome}`}>
-      <Link href="/">
-        <img src="/images/closeArticle.svg" alt="closeArticle"/>
-      </Link>
-    </div>
+    <Link href="/#showcase">
+      <div className={`${utilStyles.noSelect} ${utilStyles.backToHome}`}>
+        <img
+          src="/images/closeArticle.svg"
+          alt="closeArticle"
+        />
+      </div>
+    </Link>
   )
 }
