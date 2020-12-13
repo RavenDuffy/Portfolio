d48718a8512a4ba70872120c8d77dc29efe0b63c
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-12-13 19:49:04 +0000

    Maybe hot reloading will work eventually

--- portfolio/webpack.config.ts
+++ portfolio/webpack.config.ts
@@ -2,15 +2,24 @@ import path from 'path'
 import webpack from 'webpack'
 import webpackDevServer from 'webpack-dev-server' // needed to use .config
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import ReactRefreshTypescript from 'react-refresh-typescript'
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
 const config: webpack.Configuration = {
+  mode: isDevelopment ? 'development' : 'production',
   entry: './src/index.tsx',
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: 'ts-loader'
+        loader: 'ts-loader',
+        options: {
+          getCustomTransformers: () => ({
+            before: [ReactRefreshTypescript()]
+          })
+        }
       }
     ]
   },
@@ -21,14 +30,17 @@ const config: webpack.Configuration = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
-  plugins: [
+  plugins: isDevelopment ? [
     new ForkTsCheckerWebpackPlugin({
       async: false,
       eslint: {
         files: './src/*'
       }
-    })
-  ],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin()
+  ] : [],
+  devtool: isDevelopment ? 'eval-source-map': undefined,
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     compress: true,
