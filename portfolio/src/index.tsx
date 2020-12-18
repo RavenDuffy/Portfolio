5df3bd42efaaf8426d42f5ec5e9e340c9f520345
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2020-12-18 19:29:13 +0000

    Worked through more react docs

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -47,38 +47,70 @@ const Counter: React.FC<{}> = () => {
   )
 }
 
+const toCelsius = (fahrenheit: number): number => {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+const toFahrenheit = (celsius: number): number => {
+  return (celsius * 9 / 5) + 32;
+}
+
+const tryConversion = (temperature: string, convertFunc: Function): string => {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) return '';
+  const output = convertFunc(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 const BoilingVerdict = (props: any) => {
   return (props.celsius >= 100) ? <p>Boiling</p> : <p>Not Boiling</p>
 }
 
 type TempState = {
-  temperature: string
+  temperature: string,
+  scale: string
 }
 
 class Calculator extends React.Component<{}, TempState> {
   constructor(props: any) {
     super(props)
-    this.handleChange = this.handleChange.bind(this)
-    this.state = { temperature: '' }
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this)
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this)
+    this.state = { temperature: '', scale: 'c' }
   }
 
-  handleChange(e: any) {
-    this.setState({ temperature: e.target.value })
+  handleCelsiusChange(e: string) {
+    this.setState({ scale: 'c', temperature: e })
+  }
+
+  handleFahrenheitChange(e: string) {
+    this.setState({ scale: 'f', temperature: e })
   }
 
   render() {
-    const temperature = this.state.temperature
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConversion(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConversion(temperature, toFahrenheit) : temperature;
+
     return (
       <>
-        <TemperatureInput scale='c' />
-        <TemperatureInput scale='f' />
+        <TemperatureInput 
+          scale='c'
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput 
+          scale='f'
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict
+          celsius={parseFloat(celsius)} />
       </>
     )
   }
-}
-
-type TempInState = {
-  temperature: string;
 }
 
 type scaleNames = {
@@ -92,7 +124,13 @@ const scaleNames: scaleNames = {
   f: 'Fahrenheit'
 }
 
-class TemperatureInput extends React.Component<{scale: string}, TempInState> {
+type TemperatureProps = {
+  scale: string,
+  temperature: string,
+  onTemperatureChange: (value: string) => void
+}
+
+class TemperatureInput extends React.Component<TemperatureProps, {}> {
   constructor(props: any) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
@@ -100,11 +138,11 @@ class TemperatureInput extends React.Component<{scale: string}, TempInState> {
   }
 
   handleChange(e: any) {
-    this.setState({ temperature: e.target.value })
+    this.props.onTemperatureChange(e.target.value)
   }
 
   render() {
-    const temperature = this.state.temperature
+    const temperature = this.props.temperature
     const scale = this.props.scale
     return (
       <fieldset>
--- portfolio/webpack.config.ts
+++ portfolio/webpack.config.ts
@@ -1,10 +1,13 @@
 import path from 'path'
-import webpack from 'webpack'
-import webpackDevServer from 'webpack-dev-server' // needed to use .config
+import webpack, { web } from 'webpack'
+import webpackDevServer from 'webpack-dev-server' // provides .Configuration type
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 import { WebpackManifestPlugin } from 'webpack-manifest-plugin'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import dotenv from 'dotenv'
+
+const env = dotenv.config({path: path.resolve(__dirname, ".env.development.local")})
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 const config: webpack.Configuration = {
@@ -65,12 +68,19 @@ const config: webpack.Configuration = {
     new ForkTsCheckerWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
+      favicon: './src/images/favicon.ico',
       template: './src/index.html'
     }),
     new CleanWebpackPlugin({
       cleanAfterEveryBuildPatterns: ['public']
     }),
-    new WebpackManifestPlugin()
+    new WebpackManifestPlugin(),
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(env.parsed)
+    })
   ] : [],
   devtool: isDevelopment ? 'eval-source-map': false,
   devServer: {
