9783b8c0593859c21b5c14e0faa7247fc6ea776e
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-01-20 01:32:17 +0000

    Random Changes
    - messing around trying to find something that sticks

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,17 +1,24 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Raven Duffy | Web Designer and Computer Scientist</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className='intro'>
-          <h1>Hi, I'm <b>Raven</b></h1>
-          <h2><b>Web Designer</b> and <b>Computer Scientist</b></h2>
+        <div className='mainContainer'>
+          <div className='intro'>
+            <h1>Hi, I'm <b>Raven</b></h1>
+            <h2><b>Web Designer</b> and <b>Computer Scientist</b></h2>
+          </div>
         </div>
+        <div className='profile'>
+            <Image src='/images/me_face.png' alt='profile' layout='intrinsic' width={700} height={875} />
+          </div>
       </main>
     </>
   )
